7badd09bdbfbd6b3a045241de023c10694205ea0
HlaingYamin <95156775+HlaingYamin@users.noreply.github.com>
2022-05-28 21:17:55 +0630

    Add files via upload

---
+++ First Version/Main/ThreeDiPage/soundOnOff.js
@@ -0,0 +1,17 @@
+var audio=document.getElementById("bgAudio");
+var playPauseBtn=document.getElementById("playPausebtn");
+var count = 0;
+function playPause(){
+    if(count == 0)
+    {
+        count = 1;
+        audio.play();
+        playPauseBtn.innerHTML ="<i class='fa-solid fa-play'></i>";
+        playPauseBtn.style.fontSize = "30px";
+    }else{
+        count =0 ;
+        audio.pause();
+        playPauseBtn.innerHTML ="<i class='fa-solid fa-pause'></i>";
+        playPauseBtn.style.fontSize = "30px";
+    }
+}
